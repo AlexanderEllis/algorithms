634063a42012bfa981ceba5afbb257676069537b
Alexander Ellis <ellisonjtk@gmail.com>
2017-01-25 12:38:16 -0500

    added bubble sort and additional tests

---
+++ python/sorting_and_trees/bubble_sort.py
@@ -0,0 +1,20 @@
+# Bubble Sort
+
+def bubble_sort(input_list):
+    """
+    Input unsorted list, output sorted list
+    """
+    list_sorted = False
+    while not list_sorted:
+        changed = False
+        length = len(input_list)
+        for i in range(0, length):
+            if i != length - 1 and input_list[i] > input_list[i + 1]:
+                temp = input_list[i]
+                input_list[i] = input_list[i + 1]
+                input_list[i + 1] = temp
+                changed = True
+        if not changed:
+            list_sorted = True
+    return input_list
+
--- python/sorting_and_trees/tests.py
+++ python/sorting_and_trees/tests.py
@@ -1,4 +1,5 @@
 from merge_sort import *
+from bubble_sort import *
 from binary_search import binary_search
 import unittest
 import random
@@ -6,20 +7,30 @@ import random
 # Usage: python tests.py -v
 
 class MergeSortTest(unittest.TestCase):
-	def test_merge_sort(self):
-		self.assertEqual(merge_sort([2,4,5,1,2,4]), [1,2,2,4,4,5])
-		self.assertEqual(merge_sort([5,4,3,2,1]), [1,2,3,4,5])
-		
-		random_array = []
-		for x in range(0, 100):
-			random_array.append(random.randint(0,100))
-		
-		self.assertEqual(merge_sort(random_array), sorted(random_array))
-	
-	def test_binary_search(self):
-		self.assertTrue(binary_search([1,2,3,4,5], 4))
-		self.assertTrue(binary_search([1,4,5,8,9,11,15,23,44,51,52], 52))
-		self.assertFalse(binary_search([1,4,5,6,11,24], 3))
+    def test_merge_sort(self):
+        self.assertEqual(merge_sort([2,4,5,1,2,4]), [1,2,2,4,4,5])
+        self.assertEqual(merge_sort([5,4,3,2,1]), [1,2,3,4,5])
+                
+        random_array = []
+        for x in range(0, 100):
+            random_array.append(random.randint(0,100))
+                
+        self.assertEqual(merge_sort(random_array), sorted(random_array))
+        
+    def test_bubble_sort(self):
+        self.assertEqual(bubble_sort([2,4,5,1,2,4]), [1,2,2,4,4,5])
+
+        random_array = []
+        for x in range(0, 100):
+            random_array.append(random.randint(0, 100))
+
+        self.assertEqual(bubble_sort(random_array), sorted(random_array))
+
+                
+    def test_binary_search(self):
+        self.assertTrue(binary_search([1,2,3,4,5], 4))
+        self.assertTrue(binary_search([1,4,5,8,9,11,15,23,44,51,52], 52))
+        self.assertFalse(binary_search([1,4,5,6,11,24], 3))
 
 if __name__ == '__main__':
-	unittest.main()
+        unittest.main()
